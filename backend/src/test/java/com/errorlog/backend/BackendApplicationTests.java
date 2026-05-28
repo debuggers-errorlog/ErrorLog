9bcf6304528dcdcfbd1c56c11195e3dc0287ee56
kwonsj0825 <kwonsj0825@naver.com>
2026-05-28 17:28:57 +0900

    initial commit

---
+++ backend/src/test/java/com/errorlog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorlog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
